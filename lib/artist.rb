a0e674a12d231b5a891c7ae934fb1c2bbe44751f
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-06 00:46:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,11 +9,11 @@ class Artist
 
   attr_accessor :name
   attr_reader :songs
-  # 
+  #
   # extend Memorable::ClassMethods
   # include Memorable::InstanceMethods
-
-  extend Findable
+  # 
+  # extend Findable
   #include Paramable
 
   @@all = []
